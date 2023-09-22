39c795469f1a43293f23f8bffd70a1be4a246def
Bill Frasure <bill.frasure@gmail.com>
2023-09-22 14:40:17 -0600

    Default to TripPlanner while developing

--- src/main/scala/crestedbutte/laminar/TagsOnlyLocal.scala
+++ src/main/scala/crestedbutte/laminar/TagsOnlyLocal.scala
@@ -55,7 +55,8 @@ object TagsOnlyLocal {
         )
         .getOrElse(
 //          SpringFallLoop,
-          RtaSouthbound.fullSchedule,
+          RoundTripCalculatorComponent
+//          RtaSouthbound.fullSchedule,
         ),
     )
 
