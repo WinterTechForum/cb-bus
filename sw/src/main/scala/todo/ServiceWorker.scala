79e6a56daea7ae83e24b09a162b35d2075fcac3b
Bill Frasure <bill.frasure@gmail.com>
2023-10-10 10:30:24 -0600

    Try to fix some caching in ServiceWorker

--- sw/src/main/scala/todo/ServiceWorker.scala
+++ sw/src/main/scala/todo/ServiceWorker.scala
@@ -1,16 +1,17 @@
 package todo
 
-import org.scalajs.dom.experimental.Fetch._
+import org.scalajs.dom.experimental.Fetch.*
 import org.scalajs.dom.{NotificationOptions, ServiceWorkerGlobalScope}
 import org.scalajs.dom.ServiceWorkerGlobalScope.self
 import org.scalajs.dom.experimental.serviceworkers.{ExtendableEvent, FetchEvent}
-import org.scalajs.dom.experimental._
+import org.scalajs.dom.experimental.*
 import org.scalajs.dom.raw.MessageEvent
+//import org.scalajs.dom.window.navigator
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.scalajs.js
-import scala.scalajs.js.JSConverters._
+import scala.scalajs.js.JSConverters.*
 import scala.util.{Failure, Success}
 
 object ServiceWorker {
@@ -19,8 +20,10 @@ object ServiceWorker {
   val todoAssets: js.Array[RequestInfo] = List[RequestInfo](
     "/",
     "/index.html",
+    "/index_dev.html",
 //    "/sw-opt.js",
     "/compiledJavascript/busriderapp-opt/main.js",
+    "/compiledJavascript/cb-bus/main.js",
     "/styling/style.css",
 //    "/compiledJavascript/cb-bus-opt.js",
     "/glyphicons/svg/individual-svg/glyphicons-basic-32-bus.svg",
@@ -32,9 +35,9 @@ object ServiceWorker {
     self.addEventListener(
       "install",
       (event: ExtendableEvent) => {
-//        println(
-//          s"install: service worker with message handler installed > ${event.toString}"
-//        )
+        println(
+          s"install: service worker with message handler installed > ${event.toString}"
+        )
         event.waitUntil(toCache().toJSPromise)
       }
     )
@@ -42,9 +45,9 @@ object ServiceWorker {
     self.addEventListener(
       "activate",
       (event: ExtendableEvent) => {
-//        println(
-//          s"activate: service worker activated > ${event.toString}"
-//        )
+        println(
+          s"activate: service worker activated > ${event.toString}"
+        )
 //        invalidateCache() // TODO Do I need this at all?
 //      event.waitUntil(toCache().toJSPromise)
         self.clients.claim()
@@ -55,13 +58,19 @@ object ServiceWorker {
     self.addEventListener(
       "message",
       (event: MessageEvent) => {
-        self.registration
-        self.registration.showNotification(
-          s"This is a notification from the service worker!",
-          NotificationOptions(
-            vibrate = js.Array(100d)
-          )
-        )
+
+//        navigator.serviceWorker.getRegistration().`then`(registration => {
+//          registration.toOption.get.active.postMessage("Hello from the service worker!")
+//        }
+//        )
+
+        //        self.registration
+        //        self.registration.showNotification(
+        //          s"This is a notification from the service worker!",
+        //          NotificationOptions(
+        //            vibrate = js.Array(100d)
+        //          )
+        //        )
       }
     )
 
@@ -69,6 +78,7 @@ object ServiceWorker {
     self.addEventListener(
       "fetch",
       (event: FetchEvent) => {
+        println("Fetching: " + event.request.url)
         event.respondWith(
           fromCache(event.request)
             .recoverWith{ case error => fetch(event.request).toFuture}
@@ -111,9 +121,9 @@ object ServiceWorker {
 
   def toCache(): Future[Unit] = {
     self.caches
-      .flatMap(_.open(todoCache).toFuture.flatMap(cache =>
-        //          println("toCache: caching assets...")
-        cache.addAll(todoAssets).toFuture))
+      .flatMap(_.open(todoCache).toFuture.flatMap{cache =>
+        println("toCache: caching assets...")
+        cache.addAll(todoAssets).toFuture})
 //    self.caches
 //      .open(todoCache)
 //      .toFuture
@@ -126,11 +136,15 @@ object ServiceWorker {
     self.caches
       .map(_.`match`(request)
       .toFuture
-      .flatMap { case response: Response =>
-        if ( request.url.contains("index")) println(s"fromCache: matched request > ${request.url}")
-        Future.successful(response)
-      case other => Future.failed(new Exception("Could not find cached request"))
-      }
+        .flatMap {
+          case response: Response =>
+//            if ( request.url.contains("index"))
+            println(s"fromCache: matched request > ${request.url}")
+            Future.successful(response)
+          case other =>
+            println(s"fromCache: missed request > ${request.url}")
+            Future.failed(new Exception("Could not find cached request"))
+        }
       ).get
 
   def invalidateCache(): Unit = {
