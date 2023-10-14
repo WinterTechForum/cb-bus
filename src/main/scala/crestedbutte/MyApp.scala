fc22ec5ec520ecd1fedd90b82951cadd2a042057
Bill Frasure <bill.frasure@gmail.com>
2023-10-14 15:23:04 -0600

    Publish with less serviceworker resetting?

--- src/main/scala/crestedbutte/MyApp.scala
+++ src/main/scala/crestedbutte/MyApp.scala
@@ -47,25 +47,20 @@ object MyApp extends ZIOAppDefault {
           browser.window().navigator,
         ).serviceWorker
 
-        serviceWorker
-          .getRegistrations()
-          .`then`(registrations =>
-            println("Deleting old serviceWorkers")
-            registrations.foreach(_.unregister())
+        println("Deleting old serviceWorkers")
 
-            serviceWorker
-              .register("./sw-opt.js")
-              .toFuture
-              .onComplete {
-                case Success(registration) =>
-                  println("Registered SW: " + registration.scope)
-                  registration.update()
-                case Failure(error) =>
-                  println(
-                    s"registerServiceWorker: service worker registration failed > ${error.printStackTrace()}",
-                  )
-              },
-          )
+        serviceWorker
+          .register("./sw-opt.js")
+          .toFuture
+          .onComplete {
+            case Success(registration) =>
+              println("Registered SW: " + registration.scope)
+              registration.update()
+            case Failure(error) =>
+              println(
+                s"registerServiceWorker: service worker registration failed > ${error.printStackTrace()}",
+              )
+          }
 
 //        toServiceWorkerNavigator(
 //          browser.window().navigator,
