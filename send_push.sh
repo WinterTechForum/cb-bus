274679584ea374cff9c6789e571ceea1cfd785e5
Bill Frasure <bill.frasure@gmail.com>
2023-11-05 21:50:38 -0700

    Add script to send push notification via OneSignal, after confirming everything works in the Browser :D

---
+++ send_push.sh
@@ -0,0 +1,18 @@
+curl --request POST \
+     --url https://onesignal.com/api/v1/notifications \
+     --header "Authorization: Basic ${ONESIGNAL_API_KEY}" \
+     --header 'accept: application/json' \
+     --header 'content-type: application/json' \
+     --data '
+{
+  "app_id" : "71326ce8-7c5b-4e52-a993-cd32c0605ba9",
+  "included_segments": [
+    "Total Subscriptions"
+  ],
+  "contents": {
+    "en": "Generated from command line",
+    "es": "Generated from command line in Spanish"
+  },
+  "name": "INTERNAL_CAMPAIGN_NAME"
+}
+'
