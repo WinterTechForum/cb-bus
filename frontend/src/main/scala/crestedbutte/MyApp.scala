ad850d05900abb5586a520dee5794626e0df9fc6
Bill Frasure <bill.frasure@gmail.com>
2025-07-26 17:28:36 -0600

    Maybe, maybe get all the segment indices/swiping/deleting worked out??

--- frontend/src/main/scala/crestedbutte/MyApp.scala
+++ frontend/src/main/scala/crestedbutte/MyApp.scala
@@ -5,7 +5,8 @@ import org.scalajs.dom
 object MyApp extends App {
   println("06-07-2025 04:19 PM")
 
-  ServiceWorkerClient.registerServiceWorker()
+  // ServiceWorkerClient.registerServiceWorker() // TODO Restore before pushing
+
   val appHolder = dom.document.getElementById("landing-message")
   appHolder.innerHTML = ""
   com.raquo.laminar.api.L.render(
